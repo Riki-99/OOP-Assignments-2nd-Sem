e24c4b069db5ab90b25792a7efe93dea9661186d
unknown <rikisonrijal000@gmail.com>
2025-06-30 06:18:13 +0545

    Questions added

--- Week5/1.cpp
+++ Week5/1.cpp
@@ -1,3 +1,13 @@
+//Write a class for instantiating the objects that represent the two-dimensional Cartesian coordinate system.
+
+// A. Make a particular member function of one class as a friend function of another class for addition.
+
+// B. Make the other three functions to work as a bridge between the classes for multiplication, division, and subtraction.
+
+// C. Also write a small program to demonstrate that all the member functions of one class are the friend functions of another class if the former class is made friend to the latter.
+
+// Make least possible classes to demonstrate all the above in a single program without conflict.
+
 #include <iostream>
 using namespace std;
 
